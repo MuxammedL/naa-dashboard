67d3827afa863087d0596fb523cb80addcc61974
Muxammed Layicov <layicovmuxammed@gmail.com>
2025-10-12 18:53:06 +0400

    added sidebar

--- src/components/layout/mainLayout/index.tsx
+++ src/components/layout/mainLayout/index.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/layout/header";
+import Sidebar from "../sidebar";
 
 const MainLayout = () => {
     return (
         <div className={`relative `}>
             <Header />
             <main>
+                <Sidebar />
                 <Outlet />
             </main>
         </div>
