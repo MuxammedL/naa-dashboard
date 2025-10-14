713b5773e710ded72d7c2975d630c8992efb381c
Muxammed Layicov <layicovmuxammed@gmail.com>
2025-10-14 18:19:45 +0400

    feat: integrate TeacherStaffDetailsPanel and enhance loading state handling in TeacherTable

--- src/pages/teacherStaffPage/index.tsx
+++ src/pages/teacherStaffPage/index.tsx
@@ -1,4 +1,5 @@
 import TeacherStaffControls from "@/components/app/teacherStaffControls"
+import TeacherStaffDetailsPanel from "@/components/app/teacherStaffDetailsPanel"
 import TeacherStaffTitle from "@/components/app/teacherStaffTitle"
 import { TeacherTable } from "@/components/ui/teachersTable"
 import { columns } from "@/components/ui/teachersTable/columns"
@@ -19,17 +20,6 @@ const TeacherStaffPage = () => {
         },
     })
 
-    if (isLoading) {
-        return (
-            <div className="grid place-items-center w-full min-h-[400px]">
-                <div className="flex items-center gap-2 justify-center">
-                    <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
-                    <span>Yüklənir...</span>
-                </div>
-            </div>
-        );
-    }
-
     if (error) {
         return (
             <div className="flex flex-col w-full items-center justify-center min-h-[200px]">
@@ -41,10 +31,11 @@ const TeacherStaffPage = () => {
     }
 
     return (
-        <section className="px-6 py-4 flex flex-col gap-4 w-full">
+        <section className="px-6 py-4 flex flex-col gap-4 w-full h-full">
             <TeacherStaffTitle />
             <TeacherStaffControls />
-            <TeacherTable data={teachers} columns={columns} />
+            <TeacherTable data={teachers} columns={columns} isLoading={isLoading} />
+            <TeacherStaffDetailsPanel />
         </section>
     )
 }
